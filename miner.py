23c10d2f58056ce749eb7869ca59fc3b9d487a67
David Kuenka <cuencaldd2001.29@gmail.com>
2020-10-17 02:26:53 +0300

    Downloaded 1-8 nums. Ready for first test with outer library

--- miner.py
+++ miner.py
@@ -1,6 +1,19 @@
 import numpy as np
 import pygame
 from winconfig import *
+from PIL import ImageTk, Image
+
+
+mas = np.array(
+	[[0, 0, 1, 0, 0, 0, 0, 0], 
+	[0, 0, 0, 0, 0, 1, 0, 0], 
+	[0, 0, 0, 1, 0, 0, 0, 0], 
+	[0, 0, 0, 0, 0, 0, 1, 0], 
+	[0, 0, 1, 0, 0, 0, 1, 0], 
+	[0, 0, 0, 0, 0, 0, 1, 0], 
+	[0, 0, 0, 0, 1, 0, 0, 0], 
+	[0, 1, 0, 0, 0, 1, 0, 0]])
+
 
 
 
@@ -11,43 +24,68 @@ def screen_init(cfg):
 
 	screen = pygame.display.set_mode(
 		(cfg.cell_size * cfg.cell_quantity + 1,
-		cfg.cell_size * cfg.cell_quantity + cfg.head_hight + 1), 
-	    flags=pygame.NOFRAME)
+		cfg.cell_size * cfg.cell_quantity + cfg.head_hight + 1))
 
+	
 	pygame.display.set_caption(cfg.title)
 
 	return screen
 
 
 
+def img_load(cfg):
+	mine_image = pygame.image.load("sourse/img/mine.png").convert_alpha()
+	mine_image = pygame.transform.scale(
+		mine_image, 
+		(cfg.cell_size, cfg.cell_size))
+
+	return mine_image
+
+
+
 def game_field_drawing(cfg, screen):
 	""" Заливка окна + наложение сетки/
-	Добавление + head_high для хэд бара """
+	Добавление head_high для хэд бара """
+
+	mine_image = img_load(cfg)
 
 	screen.fill((50, 50, 50))
-	
+
 	for i in range(cfg.cell_quantity + 1):
 
 		# Columns drawing
 		pygame.draw.line(
-			screen, 
-			(255, 255, 255), 
+			screen,
+			(255, 255, 255),
 			[0, cfg.cell_size*i + cfg.head_hight], 
-			[cfg.cell_size * cfg.cell_quantity, cfg.cell_size*i + cfg.head_hight], 
+			[cfg.cell_size * cfg.cell_quantity, 
+				cfg.cell_size*i + cfg.head_hight], 
 			1)
 
-		# Rows drawing
+		# Rows drwing
 		pygame.draw.line(
 			screen, 
-			(255, 255, 255), 
-			[cfg.cell_size*i, cfg.head_hight], 
-			[cfg.cell_size*i, cfg.cell_size * cfg.cell_quantity + cfg.head_hight], 
+			(255, 255, 255),
+
+			[cfg.cell_size*i, 
+				cfg.head_hight],
+
+			[cfg.cell_size*i, 
+				cfg.cell_size * cfg.cell_quantity + cfg.head_hight], 
 			1)
+
+	for i in range(cfg.cell_quantity):
+		for j in range(cfg.cell_quantity):
+			if mas[i][j]:
+				screen.blit(
+					mine_image, 
+					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
 
 
 
 def screen_interaction(cfg, screen):
-	""" Обработка событий + вызов заливки"""
+	""" Обработка событий + вызов заливки """
+
 	clock = pygame.time.Clock()
 	game_running = True
 	while game_running:
@@ -61,12 +99,14 @@ def screen_interaction(cfg, screen):
 
 
 
-
 def main():
 	cfg = WinConfig()
 	screen = screen_init(cfg)
 	screen_interaction(cfg, screen)
 
 
+
 if __name__ == '__main__':
 	main()
+
+
--- winconfig.py
+++ winconfig.py
@@ -1,10 +1,10 @@
+import pygame
+
 class WinConfig():
 	"""docstring for WinConfig"""
 	def __init__(self):
 		self.title = "Miner"
-		self.cell_quantity = 25
+		self.cell_quantity = 8
 		self.cell_size = 25
 		self.head_hight = 50
 		self.fps = 60
-
-
