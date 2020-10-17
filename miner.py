3d917148fa9fce57802ed9daae3eaa2f9b81a64d
David Kuenka <cuencaldd2001.29@gmail.com>
2020-10-18 00:46:06 +0300

    Optimized loading of images. Ready for merge with Mechanics

--- miner.py
+++ miner.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pygame
-from winconfig import *
+import winconfig
 from PIL import ImageTk, Image
 
 
@@ -25,7 +25,7 @@ dig_matrix = np.array(
 	[1, 1, 1, 1, 2, 0, 2, 0]])
 
 open_cell_matrix = np.array(
-	[[1, 0, 1, 0, 0, 0, 0, 0], 
+	[[1, 0, 1, 0, 0, 1, 0, 0], 
 	[1, 0, 0, 0, 0, 0, 0, 0], 
 	[1, 0, 0, 0, 0, 1, 0, 0], 
 	[1, 0, 0, 0, 1, 1, 0, 0], 
@@ -53,145 +53,34 @@ def screen_init(cfg):
 
 
 
-def img_load(cfg):
-	mine_image = pygame.image.load("sourse/img/mine.png").convert_alpha()
-	mine_image = pygame.transform.scale(
-		mine_image, 
-		(cfg.cell_size, cfg.cell_size))
 
-	cell_image = pygame.image.load("sourse/img/botton.png").convert_alpha()
-	cell_image = pygame.transform.scale(
-		cell_image, 
-		(cfg.cell_size, cfg.cell_size))
-
-	first_image = pygame.image.load("sourse/img/1.png").convert_alpha()
-	first_image = pygame.transform.scale(
-		first_image, 
-		(cfg.cell_size, cfg.cell_size))
-
-	second_image = pygame.image.load("sourse/img/2.png").convert_alpha()
-	second_image = pygame.transform.scale(
-		second_image, 
-		(cfg.cell_size, cfg.cell_size))
-
-	third_image = pygame.image.load("sourse/img/3.png").convert_alpha()
-	third_image = pygame.transform.scale(
-		third_image, 
-		(cfg.cell_size, cfg.cell_size))
-
-	fourth_image = pygame.image.load("sourse/img/4.png").convert_alpha()
-	fourth_image = pygame.transform.scale(
-		fourth_image, 
-		(cfg.cell_size, cfg.cell_size))
-
-	fifth_image = pygame.image.load("sourse/img/5.png").convert_alpha()
-	fifth_image = pygame.transform.scale(
-		fifth_image, 
-		(cfg.cell_size, cfg.cell_size))
-
-	sixth_image = pygame.image.load("sourse/img/6.png").convert_alpha()
-	fifth_image = pygame.transform.scale(
-		fifth_image, 
-		(cfg.cell_size, cfg.cell_size))
-
-	seventh_image = pygame.image.load("sourse/img/7.png").convert_alpha()
-	seventh_image = pygame.transform.scale(
-		seventh_image, 
-		(cfg.cell_size, cfg.cell_size))
-
-	eighth_image = pygame.image.load("sourse/img/8.png").convert_alpha()
-	eighth_image = pygame.transform.scale(
-		eighth_image, 
-		(cfg.cell_size, cfg.cell_size))
-
-	return (mine_image, 
-		cell_image, 
-		first_image,
-		second_image,
-		third_image,
-		fourth_image,
-		fifth_image,
-		sixth_image,
-		seventh_image,
-		eighth_image)
+def game_field_drawing(cfg, screen, pics):
+	""" Заливка окна + наложение сетки """
 
 
+	screen.fill((150, 150, 150)) # Цвет бэкграунда
 
-
-def game_field_drawing(cfg, screen):
-	""" Заливка окна + наложение сетки/
-	Добавление head_high для хэд бара """
-
-	(mine_image, 
-	cell_image, 
-	first_image,
-	second_image,
-	third_image,
-	fourth_image,
-	fifth_image,
-	sixth_image,
-	seventh_image,
-	eighth_image) = img_load(cfg)
-
-	screen.fill((150, 150, 150))
 
 	for j in range(cfg.cell_quantity):
 		for i in range(cfg.cell_quantity):
 
 			if (open_cell_matrix[i][j]) == 0:
 				screen.blit(
-					cell_image, 
+					pics.cell_image, 
 					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
 
 			elif mas[i][j]:
 				screen.blit(
-					mine_image, 
+					pics.mine_image, 
 					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
 
-			elif dig_matrix[i][j] != 0:
-				if dig_matrix[i][j] == 1:
-					screen.blit(
-					first_image, 
-					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
-
-				if dig_matrix[i][j] == 2:
-					screen.blit(
-					second_image, 
-					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
-
-				if dig_matrix[i][j] == 3:
-					screen.blit(
-					third_image, 
-					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
-
-				if dig_matrix[i][j] == 4:
-					screen.blit(
-					fourth_image, 
-					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
-
-				if dig_matrix[i][j] == 5:
-					screen.blit(
-					fifth_image, 
-					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
-
-				if dig_matrix[i][j] == 6:
-					screen.blit(
-					sixth_image, 
-					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
-
-				if dig_matrix[i][j] == 7:
-					screen.blit(
-					seventh_image, 
-					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
-
-				if dig_matrix[i][j] == 8:
-					screen.blit(
-					eighth_image, 
+			elif ((dig_matrix[i][j] > 0) and (dig_matrix[i][j] < 9)):
+				screen.blit(
+					pics.digit_images_list[dig_matrix[i][j]], 
 					(i*cfg.cell_size, j*cfg.cell_size + cfg.head_hight))
 
 
 	for i in range(cfg.cell_quantity + 1):
-
 		
 		# Columns drawing
 		pygame.draw.line(
@@ -202,7 +91,7 @@ def game_field_drawing(cfg, screen):
 				cfg.cell_size*i + cfg.head_hight], 
 			1)
 
-		# Rows drwing
+		# Rows drawing
 		pygame.draw.line(
 			screen, 
 			(0, 0, 0),
@@ -221,22 +110,38 @@ def game_field_drawing(cfg, screen):
 def screen_interaction(cfg, screen):
 	""" Обработка событий + вызов заливки """
 
+	# Подгрузка изображений
+	pics = winconfig.Image(cfg)
+
 	clock = pygame.time.Clock()
 	game_running = True
 	while game_running:
 		clock.tick(cfg.fps)
+
 		for event in pygame.event.get():
+
+			# Нажатие на ячейку
+			if (event.type == pygame.MOUSEBUTTONDOWN):
+				x, y = (pygame.mouse.get_pos())
+				x //= cfg.cell_size
+				y = (y - cfg.head_hight) // cfg.cell_size
+
+				# Координата + left right button
+				print(x, y, "l" if event.button == 1 else "r") 
+
+			# Выход из игры
 			if (event.type == pygame.KEYDOWN) or (event.type == pygame.QUIT):
 				game_running = False
 			
-		game_field_drawing(cfg, screen)
+		# Зарисовка экрана
+		game_field_drawing(cfg, screen, pics)
 		pygame.display.flip()
 
 
 
 
 def main():
-	cfg = WinConfig()
+	cfg = winconfig.WinConfig()
 	screen = screen_init(cfg)
 	screen_interaction(cfg, screen)
 
--- winconfig.py
+++ winconfig.py
@@ -1,10 +1,49 @@
 import pygame
 
+
+
+def img_load(cfg):
+	""" Подгрузка объектов всех изображений """
+
+	digit_images_list = [None, ] # Список изображений цифр, начиная с 1
+
+
+	mine_image = pygame.image.load("sourse/img/mine.png").convert_alpha()
+	mine_image = pygame.transform.scale(
+		mine_image, 
+		(cfg.cell_size, cfg.cell_size))
+
+	cell_image = pygame.image.load("sourse/img/botton.png").convert_alpha()
+	cell_image = pygame.transform.scale(
+		cell_image, 
+		(cfg.cell_size, cfg.cell_size))
+
+
+	# Цикл для каждой цифры
+	for digit_image in range(1,9):
+
+		digit_images_list.append(pygame.image.load(('sourse/img/{}.png'.format(str(digit_image)))).convert_alpha())
+		digit_images_list[digit_image] = pygame.transform.scale(
+			digit_images_list[digit_image], 
+			(cfg.cell_size, cfg.cell_size))
+
+	return (mine_image, 
+		cell_image, 
+		digit_images_list)
+
+
+
 class WinConfig():
 	"""docstring for WinConfig"""
 	def __init__(self):
 		self.title = "Miner"
 		self.cell_quantity = 8
 		self.cell_size = 50
-		self.head_hight = 50
+		self.head_hight = 75
 		self.fps = 60
+
+
+
+class Image():
+	def __init__(self, cfg):
+		self.mine_image, self.cell_image, self.digit_images_list = img_load(cfg)
